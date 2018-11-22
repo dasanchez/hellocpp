c44213234b80353f82e8cf2f5a9e7eac9d708607
Dante <dante.a.sanchez@gmail.com>
2018-11-21 19:40:02 -0500

    Maze traversal

--- C++ How to Program/sandbox.cpp
+++ C++ How to Program/sandbox.cpp
@@ -1,110 +1,247 @@
 /*
- * Ex. 8.15: Quicksort
- * The basic algorithm for a single-subscripted array of values
- * is as follows:
- * a) Partitioning step: Take the first element of the unsorted
- * array and determine its final location in the sorted array
- * (i.e., all values to the left are less than the element, and
- * all values to the right are greater than the element). We now
- * have one lement in its proper location and two unsorted subarrays.
- * b) Recursive step: Perform Step 1 on each unsorted subarray.
- * Write recursive function quickSort to sort a single-subscripted
- * integer array. The function should receive as arguments an
- * integer array, a starting subscript, and an ending subscript.
- * Function partition should be called by quickSort to perform
- * the partitioning step. 
+ * Ex. 8.16: Maze Traversal
+ * The grid of hashes and dots is a two-dimensional array
+ * representation of a mze. The hashes are the walls and the
+ * dots are squares in the possible paths through the maze. 
+ * Use the following algorithm: Place your right hand on the wall
+ * to your right and begin walking forward. Never remove your hand
+ * from the wall. If the maze turns to the right, you follow
+ * the wall to the right. As long as you do not remove your
+ * hand from the wall, eventually you'll arrive at the exit
+ * of the maze.
+ * # # # # # # # # # # # #
+ * # . . . # . . . . . . #
+ * . . # . # . # # # # . # 
+ * # # # . # . . . . # . #
+ * # . . . . # # # . # . .
+ * # # # # . # . # . # . #
+ * # . . # . # . # . # . #
+ * # # . # . # . # . # . #
+ * # . . . . . . . . # . # 
+ * # # # # # # . # # # . #
+ * # . . . . . . # . . . #
+ * # # # # # # # # # # # # 
 */
 
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
-void printArray(const int[], int);
-void quickSort(int[], int, int);
-void swapElements(int[], int, int);
-int partitionArray(int[], int, int);
+class Player
+{
+  public:
+    // constructor
+    Player(int startRow, int startCol, int startDir)
+    {
+        row = startRow;
+        col = startCol;
+        direction = startDir;
+    }
+
+    int getRow()
+    {
+        return row;
+    };
+
+    int getCol()
+    {
+        return col;
+    };
+
+    int getDirection()
+    {
+        return direction;
+    };
+
+    void rotate(bool cw)
+    {
+        if (cw) // clockwise
+        {
+            if (direction == 3)
+            {
+                direction = 0;
+            }
+            else
+                direction++;
+        }
+        else
+        {
+            if (direction == 0)
+            {
+                direction = 3;
+            }
+            else
+                direction--;
+        }
+    };
+
+    void advance()
+    {
+        // move in the direction specified
+        switch (direction)
+        {
+        case 0: // up
+            row--;
+            break;
+        case 1: // right
+            col++;
+            break;
+        case 2: // down
+            row++;
+            break;
+        case 3: // left
+            col--;
+            break;
+        default:
+            break;
+        }
+    };
+
+  private:
+    int row;
+    int col;
+    int direction; // 0: up, 1: right, 2: down, 3: left
+};
+
+const int rows = 12;
+const int cols = 12;
+void printMaze(const bool maze[rows][cols], const int, const int, const int);
+int moveOptions(const bool maze[rows][cols], Player);
 
 int main()
 {
-    const int arraySize = 10;
-    int numberArray[arraySize];
 
-    cout << "Enter " << arraySize << " numbers: ";
-    for (int i =0; i< arraySize; ++i)
-        cin >>  numberArray[i];
-    
-    cout << "Before sorting:" << endl;
-    printArray(numberArray, sizeof(numberArray) / sizeof(int));
-    quickSort(numberArray, 0, sizeof(numberArray) / sizeof(int));
-    cout << "After sorting:" << endl;
-    printArray(numberArray, sizeof(numberArray) / sizeof(int));
-}
+    const bool maze[rows][cols] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+        1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0,
+        0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1,
+        0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+        0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void quickSort(int array[], int start, int end)
-{
-    int pivot = partitionArray(array, start, end);
-    if (pivot - start > 1) {
-        quickSort(array, start, pivot);
-    }
-    if (end - (pivot + 1) > 1) {
-        quickSort(array, pivot + 1, end);
-    }
-}
+    Player player(2, 0, 1);
 
-int partitionArray(int array[], int start, int end)
-{
-    // find the right location the first element of the array
-    // by swapping elements left and right, and return
-    // the index of the new location
-    bool swap = true;
-    int leftLimit = start;
-    int rightLimit = end - 1;
-    int pivotIndex = start;
+    printMaze(maze, player.getRow(), player.getCol(), player.getDirection());
 
-    while (swap)
+    while (true)
     {
-        // keep running until no swaps occur
-        swap = false;
-        // look for a smaller item starting from the rightLimit
-        for (int i = rightLimit; i > pivotIndex; --i)
+        switch (moveOptions(maze, player))
         {
-            if (array[i] < array[pivotIndex])
-            {
-                // swap elements
-                swapElements(array, i, pivotIndex);
-                leftLimit = pivotIndex + 1;
-                pivotIndex = i;
-                swap = true;
-            }
+        case 1: // go forward
+            player.advance();
+            break;
+        case 2: // turn right and advance
+            player.rotate(true);
+            player.advance();
+            break;
+        case 3: // turn left and advance
+            player.rotate(false);
+            player.advance();
+            break;
+        case 0: // turn around
+            player.rotate(true);
+            player.rotate(true);
+            break;
         }
-        // look for a larger item starting from the leftLimit
-        for (int j = leftLimit; j < pivotIndex; ++j)
+        printMaze(maze, player.getRow(), player.getCol(), player.getDirection());
+
+        if (player.getRow() == 0 || player.getRow() == 11 || player.getCol() == 0 || player.getCol() == 11)
         {
-            if (array[j] > array[pivotIndex])
-            {
-                // swap elements
-                swapElements(array, j, pivotIndex);
-                rightLimit = pivotIndex - 1;
-                pivotIndex = j;
-                swap = true;
-            }
+            cout << "Maze cleared!" << endl;
+            break;
         }
     }
-    return pivotIndex;
 }
 
-void swapElements(int array[], int first, int second)
+int moveOptions(const bool maze[rows][cols], Player player)
 {
-    int temp = array[first];
-    array[first] = array[second];
-    array[second] = temp;
+    // returns:
+    // 0 if there is no way forward or sideways
+    // 1 if we can go forward
+    // 2 if we can go right
+    // 3 if we can go left
+    switch (player.getDirection())
+    {
+    case 0: // facing up
+        if (maze[player.getRow()][player.getCol() + 1])
+            return 2; // turn right
+        else if (maze[player.getRow() - 1][player.getCol()])
+            return 1; // go forward
+        else if (maze[player.getRow()][player.getCol() - 1])
+            return 3; // turn left
+        break;
+    case 1: // facing right
+        if (maze[player.getRow() + 1][player.getCol()])
+            return 2; // turn right
+        else if (maze[player.getRow()][player.getCol() + 1])
+            return 1; // go forward
+        else if (maze[player.getRow() - 1][player.getCol()])
+            return 3; // turn left
+        break;
+    case 2: // facing down
+        if (maze[player.getRow()][player.getCol() - 1])
+            return 2; // turn right
+        else if (maze[player.getRow() + 1][player.getCol()])
+            return 1; // go forward
+        else if (maze[player.getRow()][player.getCol() + 1])
+            return 3; // turn left
+        break;
+    case 3: // left
+        if (maze[player.getRow() - 1][player.getCol()])
+            return 2; // turn right
+        else if (maze[player.getRow()][player.getCol() - 1])
+            return 1; // go forward
+        else if (maze[player.getRow() + 1][player.getCol()])
+            return 3; // turn left
+        break;
+    default:
+        break;
+    }
+    return 0;
 }
 
-void printArray(const int array[], int arraySize)
+void printMaze(const bool maze[rows][cols], const int playerRow, const int playerCol, const int playerDir)
 {
-    for (int i = 0; i < arraySize; ++i)
-        cout << array[i] << " ";
-    cout << endl;
+    cout << "=======================" << endl;
+
+    for (int row = 0; row < rows; ++row)
+    {
+        for (int col = 0; col < cols; ++col)
+        {
+            if (row == playerRow && col == playerCol)
+            {
+                switch (playerDir)
+                {
+                case 0:
+                    cout << "^ ";
+                    break;
+                case 1:
+                    cout << "> ";
+                    break;
+                case 2:
+                    cout << "v ";
+                    break;
+                case 3:
+                    cout << "< ";
+                    break;
+                default:
+                    break;
+                }
+            }
+            else if (maze[row][col] == 0)
+            {
+                cout << "# ";
+            }
+            else
+            {
+                cout << ". ";
+            }
+        }
+        cout << endl;
+    }
 }
