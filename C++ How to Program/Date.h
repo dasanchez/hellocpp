8d1a70b8b831bbe3ab456abaca2e1f108ef3539a
Dante <dante.a.sanchez@gmail.com>
2019-01-03 15:12:55 -0500

    10.3: Composition: Objects as Members of Classes

--- C++ How to Program/Date.h
+++ C++ How to Program/Date.h
@@ -1,4 +1,4 @@
-// Fig 9.17: Date.h
+// Fig 10.8: Date.h
 
 #ifndef DATE_H
 #define DATE_H
@@ -7,18 +7,18 @@
 class Date
 {
   public:
+    static const int monthsPerYear = 12; // number of months in a year
     Date(int = 2000, int = 1, int = 1); // default constructor
-
-    void print();
-    void nextDay();
-    bool isLeapYear(const int);
-    int daysInMonth(const int month, const int year);
+    void print() const; // print date in month/day/year format
+    ~Date();
 
   private:
-    int month;
-    int day;
-    int year;
+    int month; // 1-12 (January-December)
+    int day; // 1-31 based on month
+    int year; // any year
 
+    // utility function to check if day is proper for month and year
+    int checkDay(int) const;
 }; // end class Date
 
 #endif
---
+++ C++ How to Program/Employee.cpp
@@ -0,0 +1,36 @@
+// Fig 10.11: Employee.cpp
+#include <iostream>
+#include "Employee.h"
+#include "Date.h"
+using namespace std;
+
+// constructor uses member initializer list to pass initializer
+// values to constructors of member objects
+Employee::Employee(const string &first, const string &last,
+                   const Date &dateOfBirth, const Date &dateOfHire)
+    : firstName(first),       // initialize firstName
+      lastName(last),         // initialize lastName
+      birthDate(dateOfBirth), // initialize birthDate
+      hireDate(dateOfHire)    // initialize hireDate
+{
+    // output Employee object to show  when constructor is called
+    cout << "Employee object constructor: "
+         << firstName << ' ' << lastName << endl;
+} // end Employee constructor
+
+// print Employee object
+void Employee::print() const
+{
+    cout << lastName << ", " << firstName << " Hired: ";
+    hireDate.print();
+    cout << " Birthday: ";
+    birthDate.print();
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+Employee::~Employee()
+{
+    cout << "Employee object destructor: "
+         << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
