37bfc6e628a06cd86c293d4100f9942a8ed9ff18
Dante <dante.a.sanchez@gmail.com>
2020-03-02 21:16:16 -0500

    20.5: Stacks

---
+++ C++ How to Program/Chapter_20/Stackcomposition.h
@@ -0,0 +1,41 @@
+// Fig 20.15: Stackcomposition.h
+// Template Stack class deinfition with ccomposed List object.
+#ifndef STACKCOMPOSITION_H
+#define STACKCOMPOSITION_H
+
+#include "List.h" // List class definition
+
+template <typename STACKTYPE>
+class Stack
+{
+public:
+    // no constructor; List constructor does initialization
+
+    // push calls stackList obkect's insertAtFront member function
+    void push(const STACKTYPE &data)
+    {
+        stackList.insertAtFront(data);
+    } // end function push
+
+    // pop calls stackList object's removeFrontFront member function
+    bool pop(STACKTYPE &data)
+    {
+        return stackList.removeFromFront(data);
+    } // end function pop
+
+    // isStackEmpty calls stackList object's isEmpty member function
+    bool isStackEmpty() const
+    {
+        return stackList.isEmpty();
+    } // end function isStackEmpty
+
+    // printStack calls stackList object's print member function
+    void printStack() const
+    {
+        stackList.print();
+    } // end function printStack
+private:
+    List<STACKTYPE> stackList; // composed List object
+};                             // end class Stack
+
+#endif
