170169c1f1029288d71b08f8c64d7bf5c5987af1
Dante <dante.a.sanchez@gmail.com>
2020-09-06 14:52:10 -0400

    Routing for diffferent files

--- Web Servers and APIs using C++/Chapter_3/crow_volume/main.cpp
+++ Web Servers and APIs using C++/Chapter_3/crow_volume/main.cpp
@@ -2,21 +2,58 @@
 using namespace std;
 using namespace crow;
 
+void sendFile(response &res, string filename, string contentType) {
+    ifstream in("../public/" + filename, ifstream::in);
+    cout << "INSIDE CROW ROUTE" << endl;
+    if(in) {
+        ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        res.set_header("Content-Type", contentType);
+        res.write(contents.str());
+    } else {
+        res.code = 404;
+        res.write("FILE NOT FOUND!");
+    }
+    res.end();
+}
+
+void sendHtml(response &res, string filename) {
+    sendFile(res, filename + ".html", "text/html");
+}
+
+void sendImage(response &res, string filename) {
+    sendFile(res, "images/" + filename, "image/jpeg");
+}
+
+void sendScript(response &res, string filename) {
+    sendFile(res, "scripts/" + filename, "text/javascript");
+}
+
+void sendStyle(response &res, string filename) {
+    sendFile(res, "styles/" + filename, "text/css");
+}
+
 int main(int argc, char* argv[]) {
     SimpleApp app;
 
+    CROW_ROUTE(app, "/styles/<string>")
+    ([](const request &req, response &res, string filename){
+        sendStyle(res, filename);
+    });
+
+    CROW_ROUTE(app, "/scripts/<string>")
+    ([](const request &req, response &res, string filename){
+        sendScript(res, filename);
+    });
+
+    CROW_ROUTE(app, "/images/<string>")
+    ([](const request &req, response &res, string filename){
+        sendImage(res, filename);
+    });
+
     CROW_ROUTE(app, "/") ([](const request &req, response &res){
-        ifstream in("../public/index.html", ifstream::in);
-        cout << "INSIDE CROW ROUTE" << endl;
-        if(in) {
-            ostringstream contents;
-            contents << in.rdbuf();
-            in.close();
-            res.write(contents.str());
-        } else {
-            res.write("FILE NOT FOUND!");
-        }
-        res.end();
+        sendHtml(res, "index");
     });
 
     char* port = getenv("PORT");
